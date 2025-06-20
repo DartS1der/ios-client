6e03ce85a086c96a4308645b955696c0ca146085
DartS1der <126813579+DartS1der@users.noreply.github.com>
2025-06-21 01:29:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
-import Register from './pages/Register' // <== Импортируешь компонент
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Settings from './pages/Settings'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Register />} /> {/* Показываем Register на главной */}
+      <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   </BrowserRouter>
 )
