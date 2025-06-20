035eb5a0511a025d7b5a232e946e090ab05c8397
DartS1der <126813579+DartS1der@users.noreply.github.com>
2025-06-21 00:39:03 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1 className="text-3xl font-bold underline">IoT Client Works!</h1>} />
+    </Routes>
+  </BrowserRouter>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
