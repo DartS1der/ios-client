17b80f42053efcea1e5f48b2c1a128cbd0d996ad
DartS1der <126813579+DartS1der@users.noreply.github.com>
2025-06-21 01:35:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
 import './index.css'
 
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Settings from './pages/Settings'
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
